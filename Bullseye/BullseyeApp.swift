c62aa8d3cce80d38225d907ade33f4701f6b6265
margixiques <margixiques@gmail.com>
2022-09-08 11:22:51 -0500

    first commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Margarita Xiques on 7/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
