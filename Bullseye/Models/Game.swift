7e81077881e970e0a0b757d2128a1d64ed4b6c5c
margixiques <margixiques@gmail.com>
2022-09-19 18:19:26 -0500

    added bonus to points function

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -13,22 +13,25 @@ struct Game {
     var round = 1
     
     func points(sliderValue: Int) -> Int {
-        100 - abs(target - sliderValue) + bonus(sliderValue: sliderValue)
-    }
-    
-    func bonus(sliderValue: Int) -> Int {
-        if target == sliderValue {
-            return 100
-        } else if abs(target - sliderValue) == 1 {
-            return 50
-        } else {
-            return 0
+        let difference = abs(target - sliderValue)
+        var bonus = 0
+        if difference == 0 {
+            bonus = 100
+        } else if difference <= 2 {
+            bonus =  50
         }
+        return 100 - difference + bonus
     }
     
-   mutating func startNewRound(points: Int) {
-       score += points
-       round += 1
-       target = Int.random(in: 1...100)
+    mutating func startNewRound(points: Int) {
+        score += points
+        round += 1
+        target = Int.random(in: 1...100)
+    }
+    
+    mutating func restart() {
+        target = Int.random(in: 1...100)
+        score = 0
+        round = 1
     }
 }
